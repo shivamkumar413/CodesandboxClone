7e36ce6f1e28ba990bb46da0704f5c209babf29d
Shivam Kumar <bhardwajshivam026@gmail.com>
2025-11-13 16:28:31 +0530

    Added Socket setup and CRUD operations on files using websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,29 @@
 import express from 'express'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 import cors from 'cors'
 import { PORT } from './config/server.config.js'
 import apiRouter from './routes/index.js'
+import chokidar from 'chokidar'
+import { handleEditorSocketEvents } from './socketHandlers/editorHandlers.js'
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server,{
+    cors : {
+        origin : '*',
+        method : ['GET','POST']
+    }
+})
+
+
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
+
+// io.on('connection',(socket)=>{
+//     console.log("A user connected")
+// })
 
 const router = express.Router()
 
@@ -18,7 +35,41 @@ app.get('/ping',(req,res)=>{
     })
 })
 
-app.listen(PORT,()=>{
+const editorNamespace = io.of('/editor')
+
+editorNamespace.on('connection',(socket)=>{
+    console.log("Editor connected");
+
+    console.log("Socket query log : ",socket.handshake.query)
+    let projectId = socket.handshake.query.projectId
+    console.log("Project id : ",projectId)
+
+    if(projectId){
+        var watcher = chokidar.watch(`projects/${projectId}`,{
+            ignored : (path)=>path.includes('node_modules'),
+            persistent : true, // Keeps the watcher in running state till the app is running
+            awaitWriteFinish : {
+                stabilityThreshold : 2000, //Ensures stability of files before triggering event
+            },
+            ignoreInitial : true, // Ignores the initial files in the directory
+        })
+
+        watcher.on("all",(event,path)=>{
+            console.log(event,path);
+            
+        })
+    }
+    
+    handleEditorSocketEvents(socket)
+
+    socket.on("disconnect",async ()=>{
+        await watcher.close();
+        console.log("Editor connected")
+    })
+
+})
+
+server.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`);
     
 })
---
+++ backend/src/socketHandlers/editorHandlers.js
@@ -0,0 +1,97 @@
+import fs from 'fs/promises'
+
+export const handleEditorSocketEvents = (socket)=>{
+    socket.on("writeFile",async (data,pathToFileOrFolder)=>{
+        try {
+            const response = await fs.writeFile(pathToFileOrFolder,data);
+            socket.emit("writeFileSuccess",{
+                data : "File written successfully"
+            })
+        } catch (error) {
+            console.log("Error writing the file ",error)
+            socket.emit("error",{
+                data : "Error writing the file"
+            })
+        }
+    })
+
+    socket.on("createFile", async ({ pathToFileOrFolder })=>{
+        const isFileAlreadyPresent = await fs.stat(pathToFileOrFolder);
+        if(isFileAlreadyPresent){
+            socket.emit("error",{
+                data : "File already exists"
+            })
+            return
+        }
+
+        try {
+            const response = await fs.writeFile(pathToFileOrFolder,"");
+            socket.emit("createFileSuccess",{
+                data : "File created successfully"
+            })
+        } catch (error) {
+            console.log("Error creating the file ",error)
+            socket.emit("error",()=>{
+                data : "Error creating the file"
+            })
+        }
+    })
+
+    socket.on("readFile",async ({pathToFileOrFolder})=>{
+        try {
+            const response = await fs.readFile(pathToFileOrFolder)
+            console.log(response.toString())
+            socket.emit("readFileSuccess",{
+                value : response.toString(),
+                path : pathToFileOrFolder
+            })
+        } catch (error) {
+            console.log("Error reading the file ",error)
+            socket.emit("error",()=>{
+                data : "Error reading the file"
+            })
+        }
+    })
+
+    socket.on('deleteFile',async ({pathToFileOrFolder})=>{
+        try {
+            const response = await fs.unlink(pathToFileOrFolder);
+            socket.on("deleteFileSuccess",{
+                data : "Successfully deleted the file"
+            })
+        } catch (error) {
+            console.log("Error deleting the file ",error)
+            socket.emit("error",{
+                data : "Error deleting the file"
+            })
+        }
+    })
+
+    socket.on("createFolder",async ({pathToFileOrFolder})=>{
+        try {
+            const response = await fs.mkdir(pathToFileOrFolder)
+            socket.on("createFolderSuccess",{
+                data : "Folder created successfully"
+            })
+        } catch (error) {
+            console.log("Error creating folder ",error);
+            socket.on("error",{
+                message : "Error creating the folder"
+            })
+        }
+    })
+
+    socket.on("deleteFolder",async ({pathToFileOrFolder})=>{
+        try {
+            const response = await fs.rmdir(pathToFileOrFolder,{recursive : true})
+            socket.on("deleteFolderSuccess",{
+                data : "Deleted folder successfully"
+            })
+        } catch (error) {
+            console.log("Error deleting the folder ",error)
+            socket.on("error",{
+                message : "Error deleting the folder"
+            })
+        }
+    })
+}   
