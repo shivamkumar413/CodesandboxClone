a59f8ef25e711a110c727378552528d7bd0029b1
Shivam Kumar <bhardwajshivam026@gmail.com>
2025-11-26 17:06:19 +0530

    Feat : Added terminal using xterm and xterm attach addon for seamless interaction

--- frontend/src/components/molecules/BrowserTerminal/BrowserTermial.jsx
+++ frontend/src/components/molecules/BrowserTerminal/BrowserTermial.jsx
@@ -1,8 +1,8 @@
 import { Terminal } from "@xterm/xterm"
 import { FitAddon } from "@xterm/addon-fit"
+import { AttachAddon } from "@xterm/addon-attach"
 import '@xterm/xterm/css/xterm.css'
 import { useEffect, useRef } from "react"
-import { io } from "socket.io-client"
 import { useParams } from "react-router-dom"
 
 export const BrowserTerminal = ()=>{
@@ -15,7 +15,7 @@ export const BrowserTerminal = ()=>{
         const term = new Terminal({
             cursorBlink : true,
             fontSize : 16,
-            fontFamily : "Ubuntu Mono",
+            fontFamily : "Fira Code",
             theme : {
                 background : "black",
                 foreground : "#ffffff",
@@ -29,32 +29,28 @@ export const BrowserTerminal = ()=>{
         term.loadAddon(fitAddon)
         fitAddon.fit()
 
-        socket.current = io(`${import.meta.env.VITE_BACKEND_URL}/terminal`,{
-            query : {
-                projectId : projectIdFromUrl
-            }
-        })
+        // socket.current = io(`${import.meta.env.VITE_BACKEND_URL}/terminal`,{
+        //     query : {
+        //         projectId : projectIdFromUrl
+        //     }
+        // })
 
-        socket.current.on("shell-output",(data)=>{
-            console.log("shell output ",data)
-            term.write(data)
-        })
+        socket.current = new WebSocket(`ws://localhost:3000/terminal?projectId=${projectIdFromUrl}`)
 
-        term.onData((data)=>{
-            console.log("clicked : ",data)
-            socket.current.emit("shell-input",data)
-        })
+        socket.current.onopen = ()=>{
+            const attachAddOn = new AttachAddon(socket.current)
+            term.loadAddon(attachAddOn)
+        }
 
         return ()=>{
             term.dispose()
-            socket.current.disconnect()
         }
     },[])
 
     return(
         <div 
             ref={terminalRef}
-            className=" h-[30vh] overflow-auto border-t border-gray-200"
+            className=" h-[30vh] w-full overflow-auto border-t border-gray-200"
             id="terminal-container"
         >
         
